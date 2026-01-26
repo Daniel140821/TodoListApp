12aa5f1c791dac7364e714f6b183b4963a8e8037
Daniel140821 <138580504+Daniel140821@users.noreply.github.com>
2026-01-26 15:49:24 +0800

    Initial commit

---
+++ forGithub/forGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  forGithubApp.swift
+//  forGithub
+//
+//  Created by Daniel on 25/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct forGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ forGithubTests/forGithubTests.swift
@@ -0,0 +1,17 @@
+//
+//  forGithubTests.swift
+//  forGithubTests
+//
+//  Created by Daniel on 25/1/2026.
+//
+
+import Testing
+@testable import forGithub
+
+struct forGithubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
