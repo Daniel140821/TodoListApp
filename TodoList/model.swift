5195878bdeb8d13802061c2f37738fd7bef6cfb7
Daniel140821 <138580504+Daniel140821@users.noreply.github.com>
2026-01-27 10:36:53 +0000

    test4

--- TodoList/model.swift
+++ TodoList/model.swift
@@ -4,8 +4,15 @@ import SwiftData
 @Model
 final class ToDoItem {
     // SwiftData 會自動將屬性映射到資料庫欄位
-    var id: UUID()
+    var id: UUID
     var title: String
     var isCompleted: Bool
-    var createdAt: Date() // 建議加上時間，方便排序
+    var createdAt: Date // 建議加上時間，方便排序
+
+    init(title: String, isCompleted: Bool = false) {
+        self.id = UUID()
+        self.title = title
+        self.isCompleted = isCompleted
+        self.createdAt = Date()
+    }
 }
