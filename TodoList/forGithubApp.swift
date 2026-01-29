73bd56000fbe0ef7906cb9d197c07301dd8e2479
Daniel140821 <138580504+Daniel140821@users.noreply.github.com>
2026-01-29 07:15:18 +0000

    13579

--- TodoList/forGithubApp.swift
+++ TodoList/forGithubApp.swift
@@ -13,7 +13,7 @@ struct TodoListApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .modelContainer(for: ToDoItem)
+                .modelContainer(for: self.ToDoItem)
         }
     }
 }
