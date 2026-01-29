347d02ab4b342a0a92001a9e0254be206c829cd8
Daniel140821 <138580504+Daniel140821@users.noreply.github.com>
2026-01-29 07:30:55 +0000

    1efwf

--- TodoList/ContentView.swift
+++ TodoList/ContentView.swift
@@ -16,12 +16,22 @@ struct ContentView: View {
     private var items: [ToDoItem]
 
 
-    private func deleteItems(offsets: IndexSet) {
-
-        for index in offsets {
-            modelContext.delete(items[index]) // 刪除數據
+    func deleteItemByUUID(uuid: UUID) {
+        // 先通过 UUID 找到对应的项目
+        if let itemToDelete = items.first(where: { $0.id == uuid }) {
+            modelContext.delete(itemToDelete)
+            // 可选：立即保存更改
+            try? modelContext.save()
         }
+    }
 
+    // 适配 List 自带的批量删除
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            for index in offsets {
+                modelContext.delete(items[index])
+            }
+        }
     }
     
     var body: some View {
@@ -33,10 +43,11 @@ struct ContentView: View {
                             .strikethrough(item.isCompleted)
 
                         Button("Done!"){
-                            item.isCompleted.toggle()
-                            for index in offsets {
-                                modelContext.delete(items[index]) // 刪除數據
-                            }           
+                            withAnimation(.easeInOut(duration: 1.0)){
+                                item.isCompleted.toggle()
+                            }completion: {
+                                deleteItemByUUID(uuid:item.id)
+                            }
                         }
                     }.onDelete(perform: deleteItems)
                 }
